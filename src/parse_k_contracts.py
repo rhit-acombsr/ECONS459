40ca3ddeae29756cdee31f086131441bcaa45156
Stephen Acomb <83610865+rhit-acombsr@users.noreply.github.com>
2024-02-04 09:06:30 -0500

    Added Saving & Loading of Contracts Lists as JSON

--- src/parse_k_contracts.py
+++ src/parse_k_contracts.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from tkinter import filedialog
 import tkinter as tk
+import json
 
 def get_data_path():
     filetypes = (("CSV files", "*.csv"), ("All files", "*.*"))
@@ -92,14 +93,46 @@ def add_expiration_dates_to_contracts(contracts_list, expiration_dates_csv_path)
 # Example usage:
 # updated_contracts_list = add_expiration_dates_to_contracts(contracts_list, 'path_to_expiration_dates_csv.csv')
 
+def get_contracts_in_path():
+    filetypes = (("Contract List JSON", "*.json"), ("All files", "*.*"))
+    path = filedialog.asksaveasfilename(filetypes=filetypes)
+    return path
+
+def get_contracts_out_path():
+    filetypes = (("Contract List JSON", "*.json"), ("All files", "*.*"))
+    path = filedialog.askopenfilename(filetypes=filetypes)
+    return path
+
+def save_contracts_to_json(contracts_list, output_path):
+    with open(output_path, 'w') as json_file:
+        json.dump(contracts_list, json_file, indent=4)
 
 # Example usage:
-csv_path = get_data_path()
-contracts_list = process_futures_data(csv_path)
-# print(str(type(contracts_list))) #list
-# print(str(type(contracts_list[0]))) #dict
-# print(str(contracts_list[0])) #it works!
-csv_path = get_data_path()
-updated_contracts_list = add_expiration_dates_to_contracts(contracts_list, csv_path)
-# print(str(updated_contracts_list[0])) #it works!
+# save_contracts_to_json(contracts_list, 'path_to_output.json')
+
+def load_contracts_from_json(input_path):
+    with open(input_path, 'r') as json_file:
+        contracts_list = json.load(json_file)
+    return contracts_list
+
+# Example usage:
+# contracts_list = load_contracts_from_json('path_to_input.json')
+
+# # Example usage:
+# csv_path = get_data_path()
+# contracts_list = process_futures_data(csv_path)
+# # print(str(type(contracts_list))) #list
+# # print(str(type(contracts_list[0]))) #dict
+# # print(str(contracts_list[0])) #it works!
+# # print(str(updated_contracts_list[0])) #it works!
+# csv_path = get_data_path()
+# updated_contracts_list = add_expiration_dates_to_contracts(contracts_list, csv_path)
+
+# list_save_path = get_contracts_in_path() #saved_contracts_list.json
+# save_contracts_to_json(updated_contracts_list,list_save_path)
+
+list_load_path = get_contracts_out_path()
+loaded_contracts_list = load_contracts_from_json(list_load_path)
+# print(str(loaded_contracts_list[0])) #it works!
+
 print("done")
